8b3bee211d9161906bca3ef05795546afeb4494f
Emanuel Moecklin <contact@1gravity.com>
2016-06-10 11:45:20 -0400

    On Samsung devices with 4.4.x the query returns an empty list but the Intent is still received and processed. Maybe Samsung doesn't allow to query/resolve its internal BroadcastReceivers.

--- ShortcutBadger/src/main/java/me/leolin/shortcutbadger/util/BroadcastHelper.java
+++ ShortcutBadger/src/main/java/me/leolin/shortcutbadger/util/BroadcastHelper.java
@@ -2,18 +2,18 @@ package me.leolin.shortcutbadger.util;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-
-import java.util.List;
 
 /**
  * Created by mahijazi on 17/05/16.
  */
 public class BroadcastHelper {
 	public static boolean canResolveBroadcast(Context context, Intent intent) {
-		PackageManager packageManager = context.getPackageManager();
+		// On Samsung devices with 4.4.x the query returns an empty list but the Intent is still
+		// received and processed. Maybe Samsung doesn't allow to query/resolve its internal
+		// BroadcastReceivers...
+		/*PackageManager packageManager = context.getPackageManager();
 		List<ResolveInfo> receivers = packageManager.queryBroadcastReceivers(intent, 0);
-		return receivers != null && receivers.size() > 0;
+		return receivers != null && receivers.size() > 0;*/
+		return true;
 	}
 }
